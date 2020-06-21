f39658ac4d7f3578656a9fb378efb3ecec6d3702
Pleasant Codes <hellopleasantcodes@gmail.com>
2020-06-21 19:35:03 +0100

    facing react issues

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -18,6 +18,7 @@ export default (state = initialState, action) => {
                 return {
                     ...state
                 }
+                default:
         return state;
     }
 }
